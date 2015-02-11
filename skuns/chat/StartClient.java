acc9cd4e26f03e8bfc3d451f270002027f4d09a5
SkunS <skunshd@gmail.com>
2015-02-11 20:47:25 +0300

    First home commit

---
+++ skuns/chat/StartClient.java
@@ -0,0 +1,10 @@
+package skuns.chat;
+
+import skuns.chat.client.ClientChat;
+
+public class StartClient {
+
+	public static void main(String[] args) {
+		ClientChat client = new ClientChat();
+	}
+}
